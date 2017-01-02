1b480f7a96454c0c34d919bdcc45af3bd65b1a4d
DESKTOP-7EGDPIC\zhuminh <821579706@qq.com>
2017-01-02 19:26:23 +0800

    1.基本完成了

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,132 @@
 #include <jni.h>
 #include <string>
+#include<stdio.h>
+#include<unistd.h>
+#include<pthread.h>
+struct NativeWorkerArgs{
+    jint id;
+    jint iterations;
+};
+static jmethodID gOnNativeMessage = NULL;
+static JavaVM* gVm=NULL;
+static jobject gObj=NULL;
+static pthread_mutex_t mutex;
+jint JNI_OnLoad(JavaVM *vm,void *reserved){
+    gVm=vm;
+    return JNI_VERSION_1_4;
+}
 
 extern "C"
-jstring
-Java_com_minhuizhu_thread_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+void Java_com_minhuizhu_thread_MainActivity_nativeInit(JNIEnv *env, jobject instance) {
+
+   if(0!=pthread_mutex_init(&mutex,NULL)){
+        jclass exceptionClazz=env->FindClass("java/lang/RuntimeException");
+       env->ThrowNew(exceptionClazz,"Unable to initialize mutex");
+       goto exit;
+   }
+    if(NULL==gObj){
+        gObj=env->NewGlobalRef(instance);
+        if(NULL==gObj){
+            goto exit;
+        }
+    }
+    if(NULL==gOnNativeMessage){
+        jclass clazz=env->GetObjectClass(instance);
+        gOnNativeMessage=env->GetMethodID(clazz,"onNativeMessage","(Ljava/lang/String;)V");
+        if(NULL==gOnNativeMessage){
+            jclass exceptionClazz=env->FindClass("java/lang/RuntimeException");
+            env->ThrowNew(exceptionClazz,"Unable to find method");
+        }
+    }
+    exit:return;
 }
+extern "C"
+void Java_com_minhuizhu_thread_MainActivity_nativeWorker(JNIEnv *env, jobject instance, jint id,
+                                                    jint iterations) {
+
+   if(0!=pthread_mutex_lock(&mutex)){
+       jclass exceptionClazz=env->FindClass("java/lang/RuntimeException");
+       env->ThrowNew(exceptionClazz,"Unable to lock mutex");
+       goto exit;
+    }
+    for(jint i=0;i<iterations;i++){
+        char message[26];
+        sprintf(message,"Worker %d: Iteration %d",id,i);
+        jstring  messageString=env->NewStringUTF(message);
+        env->CallVoidMethod(instance,gOnNativeMessage,messageString);
+        if(NULL!=env->ExceptionOccurred())
+            break;
+        sleep(1);
+    }
+    if(0!=pthread_mutex_unlock(&mutex)){
+        jclass  exceptionClazz=env->FindClass("java/lang/RuntimeException");
+        env->ThrowNew(exceptionClazz,"Unable to unlock mutex");
+    }
+    exit:return;
+}
+static void * nativeWorkerThread(void *args){
+    JNIEnv *env=NULL;
+    if(0==gVm->AttachCurrentThread(&env,NULL)){
+        NativeWorkerArgs* nativeWorkerArgs=(NativeWorkerArgs*)args;
+        Java_com_minhuizhu_thread_MainActivity_nativeWorker(env,gObj,nativeWorkerArgs->id,nativeWorkerArgs->iterations);
+        delete nativeWorkerArgs;
+        gVm->DetachCurrentThread();
+    }
+    return (void*)1;
+}
+extern "C"
+void Java_com_minhuizhu_thread_MainActivity_posixThreads(JNIEnv *env, jobject instance, jint threads,
+                                                    jint iterations) {
+for(jint i = 0;i<threads;i++){
+    pthread_t thread;
+    NativeWorkerArgs* nativeWorkerArgs=new NativeWorkerArgs();
+    nativeWorkerArgs->id=i;
+    nativeWorkerArgs->iterations=iterations;
+    int result =pthread_create(&thread,NULL,nativeWorkerThread,(void*)nativeWorkerArgs);
+    if(0!=result){
+        jclass  exceptionClazz=env->FindClass("java/lang/RuntimeException");
+        env->ThrowNew(exceptionClazz,"Unable to unlock mutex");
+    }
+}
+
+}
+extern "C"
+void Java_com_minhuizhu_thread_MainActivity_nativeFree(JNIEnv *env, jobject instance) {
+if(NULL!=gObj){
+    env->DeleteGlobalRef(gObj);
+    gObj=NULL;
+}
+if(0!=pthread_mutex_destroy(&mutex)){
+    jclass exceptionClazz=env->FindClass("java/lang/RuntimeException");
+    env->ThrowNew(exceptionClazz,"Unable to destroy mutex");
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/minhuizhu/thread/MainActivity.java
+++ app/src/main/java/com/minhuizhu/thread/MainActivity.java
@@ -1,7 +1,10 @@
 package com.minhuizhu.thread;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,19 +14,98 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    private EditText threadsEdit;
+    private EditText iterationsEdit;
+    private Button startButton;
+    private TextView logView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nativeInit();
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        threadsEdit = (EditText) findViewById(R.id.threads_edit);
+        iterationsEdit = (EditText) findViewById(R.id.iterations_edit);
+        startButton = (Button) findViewById(R.id.start_button);
+        logView = (TextView) findViewById(R.id.log_view);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                int threads = getNumber(threadsEdit, 0);
+                int iterations = getNumber(iterationsEdit, 0);
+
+                if (threads > 0 && iterations > 0) {
+                    startThreads(threads, iterations);
+                }
+            }
+        });
+    }
+    private void javaThreads(int threads,final int iterations){
+        for(int i=0;i<threads;i++){
+            final int id=i;
+            Thread thread=new Thread(){
+                @Override
+                public void run() {
+                    nativeWorker(id,iterations);
+                }
+            };
+            thread.start();
+        }
+    }
+    private native void nativeFree();
+    private native void nativeWorker(int id, int iterations);
+
+    private void startThreads(int threads, int iterations) {
+        posixThreads(threads,iterations);
     }
 
+
+
+    private int getNumber(EditText editText, int defaultValue) {
+        int num;
+        try {
+            num=Integer.parseInt(editText.getText().toString().trim());
+        }catch (Exception e){
+            num=defaultValue;
+        }
+        return num;
+    }
+
+    private native void  nativeInit() ;
+    private native void posixThreads(int threads, int iterations);
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    private void onNativeMessage(final String message){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                logView.append(message);
+                logView.append("\n");
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        nativeFree();
+        super.onDestroy();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
